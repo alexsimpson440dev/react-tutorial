00985b67686e4b7e98e5145b9d637f33c944b3ae
Alex Simpson <alexander.simpson@target.com>
2019-12-31 10:26:13 -0600

    continuously working on this tutorial

---
+++ src/components/TodoItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            background: '#f4',
+            padding: '10px',
+            borderBottom: '1px #c dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }
+    }
+
+    render() {
+        const { id, title } = this.props.todo;
+        return (
+            <div style={this.getStyle()}>
+                <input type='checkbox' onChange={this.props.markComplete.bind(this, id)} /> { ' ' }
+                <p>{ title }</p>
+            </div>
+        )
+    }
+}
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+export default TodoItem
